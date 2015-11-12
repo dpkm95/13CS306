ff4be244eec44c566f0ffdf012ace0bdc07b2904
skymanaditya1 <skymanaditya1@gmail.com>
2015-11-12 23:44:48 +0530

    C Program for testing

---
+++ testProgram.c
@@ -0,0 +1,9 @@
+	int add (int a,int b);
+    int sum (int a, int b){
+        float p = 200.0 / 20.0;
+        int r = add(40,30);
+        return r;
+    }
+    int add(int a, int b){
+    	return a+b;
+    }
